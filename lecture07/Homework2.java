10cdd2645431b342fb8c940a5d1a3fadd7f49dad
sam-edelman <70987769+sam-edelman@users.noreply.github.com>
2020-09-28 15:26:25 -0400

    conversion to minutes
    add the conversion code

--- lecture07/Homework2.java
+++ lecture07/Homework2.java
@@ -9,6 +9,21 @@ public class Homework2 {
 		System.out.println("The first time is: " + time1);
 		System.out.println("The second time is: " + time2);
 		
+		int h1, h2, m1, m2; // "12;03" 01234 (index)
+		h1 = Integer.parseInt(time1.substring(0,2));
+		m1 = Integer.parseInt(time1.substring(3));
+		h2 = Integer.parseInt(time2.substring(0,2));
+		m2 = Integer.parseInt(time2.substring(3));
+		
+		int t1 = h1 * 60 + m1; // "12:03" boils down to 12*60 + 3 == 723 min
+		int t2 = h2 * 60 + m2; //
+		
+		System.out.println("First time in minutes: " + t1);
+		System.out.println("Second time in minutes: " + t2);
+		
+		
+		
+		
 	}
 
 }
