75cfcf8bac8a003e1d614059e49c446b7c0a2886
sam-edelman <70987769+sam-edelman@users.noreply.github.com>
2020-09-28 23:50:55 -0400

    Student class completed
    added last functionality from HW03 Ex. P3.7

--- lecture09/Student.java
+++ lecture09/Student.java
@@ -17,15 +17,29 @@ public class Student {
 	    this.totalScore = this.totalScore + score; 
 	}
 	
+	// returns total score of students quizzes
+	public double getTotalScore() {
+	    return this.totalScore;
+	  }
+	
+	// returns avg score of students quizzes
+	  public double getAverageScore() {
+	    return this.totalScore / this.count; 
+	  }
+	  
 	public static void main(String[] args) {
 		Student x = new Student("Laura");
 	    System.out.println( "Student " + x + "'s name is: " +  x.getName() );
-	    x.addQuiz(89); 
-	    x.addQuiz(97);
-	    x.addQuiz(91);
+	    x.addQuiz(69); 
+	    x.addQuiz(77);
+	    x.addQuiz(21);
+	    System.out.println( x.getName() + "'s total score is: " + x.getTotalScore() ); // expected 167.0
 	    
 	    Student y = new Student("Blake");
 	    y.addQuiz(93);
+	    System.out.println( y.getName() + "'s total score is: " + y.getTotalScore() );
+	    System.out.println( x.getName() + "'s average score is: " + x.getAverageScore() ); // expected 55.6666...
+	    System.out.println( y.getName() + "'s average score is: " + y.getAverageScore() );
 	    
 	  }
 
