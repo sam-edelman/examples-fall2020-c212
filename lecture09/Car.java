ce668636e1a8c486e75ab541de08d3f55323f6b6
sam-edelman <70987769+sam-edelman@users.noreply.github.com>
2020-09-28 22:55:04 -0400

    Constructor added
    instance variables initialized as well as constructor created.

--- lecture09/Car.java
+++ lecture09/Car.java
@@ -1,6 +1,16 @@
 
 public class Car {
-
+	
+	private int efficiency; // mi/gal
+	
+	private double fuel; // gal
+	
+	// constructor
+	public Car(int efficiency) { 
+		this.efficiency = efficiency; 
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
