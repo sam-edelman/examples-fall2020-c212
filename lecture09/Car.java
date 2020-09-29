ed4ad69cc697ed14d10b4df66c06e71fa799188a
sam-edelman <70987769+sam-edelman@users.noreply.github.com>
2020-09-28 23:22:10 -0400

    Car class completed
    added last functionality for ensuring that the car doesn't run out of gas.

--- lecture09/Car.java
+++ lecture09/Car.java
@@ -11,9 +11,13 @@ public class Car {
 	}
 	
 	public void drive(double miles) { // distanced traveled in miles
-		this.fuel -=  (miles / this.efficiency);
+		double needGas = miles / this.efficiency;
+		if (this.fuel >= needGas) { // if enough gas in the tank
+			this.fuel -= needGas; 
+		}
 	}
 	
+	// fill gas for the cars gas tank
 	public void addGas(double gal) {
 		this.fuel += gal;
 	}
@@ -25,16 +29,19 @@ public class Car {
 	
 	public static void main(String[] args) {
 		Car a = new Car(50);
-		System.out.println(a);
+		
 
+		System.out.println(a.getGasInTank()); // 0.0
+		System.out.println(a.efficiency); // 50
+		
 		a.drive(100);
 		
-		System.out.println(a.getGasInTank()); // -2.0
+		System.out.println(a.getGasInTank()); // 0.0
 		System.out.println(a.efficiency); // 50
 		
 		a.addGas(3.2); // add fuel to the Gas tank
 
-		System.out.println(a.getGasInTank()); // 1.2
+		System.out.println(a.getGasInTank()); // 3.2
 		System.out.println(a.efficiency); // 50
 	}
 
