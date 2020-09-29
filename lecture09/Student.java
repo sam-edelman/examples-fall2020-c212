bc21ace9280a15727db09aa556cfff65e82fe5f9
sam-edelman <70987769+sam-edelman@users.noreply.github.com>
2020-09-28 23:35:18 -0400

    Added Constructor and instance variables.
    Beginning of ex. P3.7

---
+++ lecture09/Student.java
@@ -0,0 +1,14 @@
+
+public class Student {
+	String name; //  instance variable for students name initialized as an empty string
+
+	double totalScore; // instance variable initially 0.0 (zero)
+	int count; // instance variable initially 0 (zero)
+	
+	public Student(String name) { // constructor
+		this.name = name; // take the value of the parameter store it
+	}	
+	
+	
+
+}
