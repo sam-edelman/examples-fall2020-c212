9439aac712ca06949ba29248c71f42bc7fedd86c
sam-edelman <70987769+sam-edelman@users.noreply.github.com>
2020-09-28 23:45:40 -0400

    Added getName and addQuiz methods
    created the functionality to return the name of the student as well as add quizzes and their scores.

--- lecture09/Student.java
+++ lecture09/Student.java
@@ -2,13 +2,31 @@
 public class Student {
 	String name; //  instance variable for students name initialized as an empty string
 
-	double totalScore; // instance variable initially 0.0 (zero)
-	int count; // instance variable initially 0 (zero)
+	double totalScore; // instance variable initially 0.0
+	int count; // instance variable initially 0
 	
 	public Student(String name) { // constructor
-		this.name = name; // take the value of the parameter store it
+		this.name = name; 
 	}	
 	
+	public String getName() {
+		return this.name;
+	}
+	public void addQuiz(int score) {
+		this.count += 1; // + 1 to the number of quizzes taken
+	    this.totalScore = this.totalScore + score; 
+	}
 	
+	public static void main(String[] args) {
+		Student x = new Student("Laura");
+	    System.out.println( "Student " + x + "'s name is: " +  x.getName() );
+	    x.addQuiz(89); 
+	    x.addQuiz(97);
+	    x.addQuiz(91);
+	    
+	    Student y = new Student("Blake");
+	    y.addQuiz(93);
+	    
+	  }
 
 }
